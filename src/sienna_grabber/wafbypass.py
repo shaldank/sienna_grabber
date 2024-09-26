2fa071dc08b7a13807fb5ecdfb1c459002c4f88b
Ianko Leite <iankoleite@gmail.com>
2024-09-26 12:16:27 -0400

    update waf bypass

--- src/sienna_grabber/wafbypass.py
+++ src/sienna_grabber/wafbypass.py
@@ -1,6 +1,6 @@
 # Bypass the AWS WAF in front of the GraphQL endpoint.
+import os
 from playwright.sync_api import sync_playwright
-
 
 class WAFBypass:
     """Bypass the AWS WAF in front of the GraphQL endpoint."""
@@ -12,22 +12,30 @@ class WAFBypass:
             # Just in case the request JSON is needed later.
             # pprint(request.post_data_json)
         return request
+    
+    def get_aws_waf_token(self, context) -> None:
+        """Get the aws-waf-token from the cookies."""
+        page = context.new_page()
+        page.goto("https://www.toyota.com/all-vehicles/")
+        page.wait_for_load_state("networkidle")
+        page.close()
 
-    def get_headers(self) -> None:
+    def get_headers(self, context) -> None:
+        page = context.new_page()
+        page.on("request", self.intercept_request)
+        page.goto("https://www.toyota.com/search-inventory/")
+        page.get_by_placeholder("ZIP Code").click()
+        page.get_by_placeholder("ZIP Code").fill(os.environ.get("ZIPCODE"))
+        page.get_by_placeholder("ZIP Code").press("Enter")
+        page.wait_for_load_state("networkidle", timeout=60000)
+        page.close()
+
+    def run(self):
         """Run a browser to get valid headers for a WAF bypass."""
         with sync_playwright() as playwright:
             browser = playwright.firefox.launch(headless=True)
             context = browser.new_context(viewport={"width": 1920, "height": 1080})
-            page = context.new_page()
-            page.on("request", self.intercept_request)
-            page.goto("https://www.toyota.com/search-inventory/")
-            page.get_by_placeholder("ZIP Code").click()
-            page.get_by_placeholder("ZIP Code").fill("90210")
-            page.get_by_placeholder("ZIP Code").press("Enter")
-            page.wait_for_load_state("networkidle")
+            self.get_aws_waf_token(context)
+            self.get_headers(context)
             browser.close()
-
-    def run(self):
-        """Return the valid headers to bypass the WAF."""
-        self.get_headers()
-        return self.valid_headers
+            return self.valid_headers or None
